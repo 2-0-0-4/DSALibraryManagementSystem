48f58702eaf5d32315adb64fc6b0171633bbf767
Afeera Umair <36076765+2-0-0-4@users.noreply.github.com>
2024-03-29 12:02:27 +0500

    Add files via upload

---
+++ LibrarySystem.py
@@ -0,0 +1,30 @@
+def pswd_check(user,pswd): #checks if username and pswd exists in Userdata.csv
+    pass
+
+def log_in():
+    pass
+
+def sign_up():
+    pass
+
+def display_books(trie):
+    pass
+
+def search_book(trie):
+    pass
+
+def delete_book(trie,bookname):
+    pass
+
+def edit_book(trie,bookname):
+    pass
+
+def get_blurb(trie,bookname):
+    pass
+
+def borrow_book(trie,bookname,username):
+    # changes the flag of the book and adds bookname in UserData csv file 
+    pass
+
+def return_book(trie,bookname,username):
+    pass
